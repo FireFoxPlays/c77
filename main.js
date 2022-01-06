6837679748826c49bdddc6b53f3b7935b118ab45
FireFoxPlays <88514805+FireFoxPlays@users.noreply.github.com>
2022-01-06 14:58:44 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log("Hi I am your subconsious mind telling you that you forgot to lock your door.");
+
+var first_name="Logan ";
+console.log(first_name);
+
+var middle_name="Rain ";
+console.log(middle_name)
+
+var last_name="Van Deusen";
+console.log(last_name);
+
+var full_name=first_name+middle_name+last_name;
+console.log(full_name);
+ 
+
+var length=full_name.length;
+console.log(length);
+
+
+var upcas=full_name.toUpperCase();
+console.log(upcas);
+
+var lowcas=full_name.toLowerCase();
+console.log(lowcas);
+
+
+var minimum=Math.min(51,49,36,67,39,34);
+console.log(minimum);
+
+var maximum=Math.max(51,49,36,67,39,34);
+console.log(maximum);
